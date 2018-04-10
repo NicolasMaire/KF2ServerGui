800a81dbee087e33f493ea039b3711b8350a092f
Nicolas Maire <nmaire@ka-ra.fr>
2018-04-10 10:44:04 +0200

    Removed config saving/loading, keep it simple.

--- KF2ServerLauncher/MainWindow.xaml.cs
+++ KF2ServerLauncher/MainWindow.xaml.cs
@@ -11,17 +11,9 @@ namespace KF2ServerLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string mConfigFilePath;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            string appDataFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            string configFolderPath = Path.Combine(appDataFolderPath, "KF2Server");
-            mConfigFilePath = Path.Combine(configFolderPath, "KF2Server.xml");
-
-            LoadConfig();
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
@@ -47,90 +39,7 @@ namespace KF2ServerLauncher
 
         private void QuitButton_Click(object sender, RoutedEventArgs e)
         {
-            SaveConfig();
-
             Close();
-        }
-
-        private void SaveConfig()
-        {
-            string configFolderPath = Path.GetDirectoryName(mConfigFilePath);
-            if (!Directory.Exists(configFolderPath))
-            {
-                System.IO.Directory.CreateDirectory(configFolderPath);
-            }
-
-            FileStream writer = new FileStream(mConfigFilePath, FileMode.OpenOrCreate);
-
-            using (XmlWriter xmlWriter = XmlWriter.Create(writer))
-            {
-                xmlWriter.WriteStartElement("Config");
-                xmlWriter.WriteElementString("Map", (string)((ComboBoxItem)mapComboBox.SelectedItem).Content);
-                xmlWriter.WriteElementString("Difficulty", (string)((ComboBoxItem)difficultyComboBox.SelectedItem).Content);
-                xmlWriter.WriteElementString("MaxPlayers", (string)((ComboBoxItem)maxPlayersComboBox.SelectedItem).Content);
-                xmlWriter.WriteElementString("IsLanMatch", (isLanMatchCheckBox.IsChecked.HasValue ? true : false).ToString());
-
-                xmlWriter.WriteEndDocument();
-
-                writer.Flush();
-            }
-        }
-
-        private void LoadConfig()
-        {
-            // If config file exists, load it
-            if (File.Exists(mConfigFilePath))
-            {
-                FileStream reader = new FileStream(mConfigFilePath, FileMode.Open);
-
-                using (XmlReader xmlReader = XmlReader.Create(reader))
-                {
-                    if (xmlReader.ReadToFollowing("Map"))
-                    {
-                        string map = xmlReader.ReadElementContentAsString();
-                        foreach (ComboBoxItem item in mapComboBox.Items)
-                        {
-                            if ((string)item.Content == map)
-                            {
-                                mapComboBox.SelectedItem = item;
-
-                                break;
-                            }
-                        }
-                    }
-                    if (xmlReader.ReadToFollowing("Difficulty"))
-                    {
-                        string difficulty = xmlReader.ReadElementContentAsString();
-                        foreach (ComboBoxItem item in difficultyComboBox.Items)
-                        {
-                            if ((string)item.Content == difficulty)
-                            {
-                                difficultyComboBox.SelectedItem = item;
-
-                                break;
-                            }
-                        }
-                    }
-                    if (xmlReader.ReadToFollowing("MaxPlayers"))
-                    {
-                        string maxPlayers = xmlReader.ReadElementContentAsString();
-                        foreach (ComboBoxItem item in maxPlayersComboBox.Items)
-                        {
-                            if ((string)item.Content == maxPlayers)
-                            {
-                                maxPlayersComboBox.SelectedItem = item;
-
-                                break;
-                            }
-                        }
-                    }
-                    if (xmlReader.ReadToFollowing("IsLanMatch"))
-                    {
-                        bool isLanMatch = xmlReader.ReadElementContentAsBoolean();
-                        isLanMatchCheckBox.IsChecked = isLanMatch;
-                    }
-                }
-            }
         }
     }
 }
